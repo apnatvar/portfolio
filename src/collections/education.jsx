faaa54a32432ded7e94c2a2c903f394bc5d9c6ab
apnatvar <arawat@tcd.ie>
2025-08-08 11:49:31 +0530

    hero CSS adjustments

--- src/collections/education.jsx
+++ src/collections/education.jsx
@@ -5,13 +5,13 @@ export default function Education(){
     return(
         <section className="education glass-slab">
             <div className="college">
-                <title><Link href="/">Trinity College Dublin</Link></title>
+                <p><Link href="/">Trinity College Dublin</Link></p>
                 <p>Graduated October 2023</p>
                 <p>Bachelors in Computer Engineering</p>
                 <p>Short description</p>
             </div>
             <div>
-                <title><Link href="/">St. George's College</Link></title>
+                <p><Link href="/">St. George's College</Link></p>
                 <p>Graduated March 2018</p>
                 <p>Short Description</p>
             </div>
