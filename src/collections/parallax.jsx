3e3ec858cb910f0edae4b8742e6f28f02732e661
apnatvar <arawat@tcd.ie>
2025-08-08 13:26:33 +0530

    syntax adjustment parallax

--- src/collections/parallax.jsx
+++ src/collections/parallax.jsx
@@ -1,15 +1,14 @@
 "use client";
-import { ParallaxProvider } from 'react-scroll-parallax';
-import { Parallax } from "react-scroll-parallax";
+import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
 export default function ParallaxText({ text, speed = -10 }) {
   return (
     <ParallaxProvider>
-    <Parallax speed={speed}>
-      <span style={{ display: "inline-block", whiteSpace: "nowrap" }}>
-        {text}
-      </span>
-    </Parallax>
+        <Parallax speed={speed}>
+        <span style={{ display: "inline-block", whiteSpace: "nowrap" }}>
+            {text}
+        </span>
+        </Parallax>
     </ParallaxProvider>
   );
 }
