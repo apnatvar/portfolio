2dd0dd40e576050c9499976c278e7912a3917030
apnatvar <arawat@tcd.ie>
2025-08-08 13:22:36 +0530

    education CSS adjustments

--- src/collections/education.jsx
+++ src/collections/education.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import ParallaxText from "./parallax";
 
 export default function Education(){
     return(
         <section className="education glass-slab">
-            <div className="college">
-                <p><Link href="/">Trinity College Dublin</Link></p>
-                <p>Graduated October 2023</p>
-                <p>Bachelors in Computer Engineering</p>
-                <p>Short description</p>
+            <p className="ed-title">Education</p>
+            <div className="ed-card">
+                <p className="institute"><Link href="/">Trinity College Dublin</Link></p>
+                <p className="grad-date">Graduated October 2023</p>
+                <p className="degree">Bachelors in Computer Engineering</p>
+                <p className="ed-description">Short description</p>
+                <p className="ed-description">Short description</p>
+                <p className="ed-description">Short description</p>
             </div>
-            <div>
-                <p><Link href="/">St. George's College</Link></p>
-                <p>Graduated March 2018</p>
-                <p>Short Description</p>
+            <div className="ed-card">
+                <p className="institute"><Link href="/">St. George's College</Link></p>
+                <p className="grad-date">Graduated March 2018</p>
+                <p className="degree">Indian School Ceritificate (ISC)</p>
+                <p className="ed-description">Short Description</p>
+                <p className="ed-description">Short description</p>
+                <p className="ed-description">Short description</p>
             </div>
         </section>
     );
---
+++ src/collections/parallax.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { ParallaxProvider } from 'react-scroll-parallax';
+import { Parallax } from "react-scroll-parallax";
+
+export default function ParallaxText({ text, speed = -10 }) {
+  return (
+    <ParallaxProvider>
+    <Parallax speed={speed}>
+      <span style={{ display: "inline-block", whiteSpace: "nowrap" }}>
+        {text}
+      </span>
+    </Parallax>
+    </ParallaxProvider>
+  );
+}
