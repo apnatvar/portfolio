ca23a503e10c1928e24bef672907152cb665dfe3
apnatvar <arawat@tcd.ie>
2025-08-10 11:25:03 +0530

    added smooth scrolling to sections

--- src/collections/topSlab.jsx
+++ src/collections/topSlab.jsx
@@ -1,16 +1,19 @@
+'use client'
 import Link from "next/link";
 import React from "react";
+import useSmoothScroll from "@/hooks/smoothScroll";
 
 export default function TopSlab(){
+    const handleScrollTo = useSmoothScroll();
     return(
         <section className="bottom-slab glass-slab">
             <div className="spark"></div>
             <div className="horizontal-list">
                 <p className="horizontal-list-element"><Link target="_blank" href="https://www.linkedin.com/in/apnatva-singh-rawat/">LinkedIn</Link></p>
                 <p className="horizontal-list-element"><Link target="_blank" href="https://github.com/apnatvar/">GitHub</Link></p>
-                <p className="horizontal-list-element"><Link href="/#experience">Experience</Link></p>
-                <p className="horizontal-list-element"><Link href="/#education">Education</Link></p>
-                <p className="horizontal-list-element"><Link href="/#project">Projects</Link></p>
+                <p className="horizontal-list-element" onClick={() => handleScrollTo('experience', 2000)}>Experience</p>
+                <p className="horizontal-list-element" onClick={() => handleScrollTo('education', 2500)}>Education</p>
+                <p className="horizontal-list-element" onClick={() => handleScrollTo('projects', 3000)}>Projects</p>
             </div>
         </section>
     );
---
+++ src/hooks/smoothScroll.jsx
@@ -0,0 +1,32 @@
+import { React, useCallback } from "react";
+
+export default function useSmoothScroll() {
+  const smoothScrollTo = useCallback((id, duration = 1000) => {
+    const target = document.getElementById(id);
+    if (!target) return;
+
+    const start = window.pageYOffset;
+    const end = target.getBoundingClientRect().top + start;
+    const distance = end - start;
+    const startTime = performance.now();
+
+    function easeInOutQuad(t) {
+      return t < 0.5
+        ? 2 * t * t
+        : -1 + (4 - 2 * t) * t;
+    }
+
+    function scroll() {
+      const now = performance.now();
+      const time = Math.min((now - startTime) / duration, 1);
+      const easedTime = easeInOutQuad(time);
+      window.scrollTo(0, start + distance * easedTime);
+
+      if (time < 1) requestAnimationFrame(scroll);
+    }
+
+    requestAnimationFrame(scroll);
+  }, []);
+
+  return smoothScrollTo;
+}
