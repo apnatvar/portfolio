ba288ee979dfdc6017bc87eef5aaac1d2cfd0af7
apnatvar <arawat@tcd.ie>
2025-08-27 16:25:15 +0530

    added sefadein for text in new cards

--- src/components/minExperience.jsx
+++ src/components/minExperience.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ScrollReveal from "./verticalScrollReveal";
 import SlideInText from "./slideInAnimation";
 import TiltCard from "./gridAnimation";
+import SequentialFadeIn from "./sequentialFadeIn";
 
 export default function MinExperience() {
     return (
@@ -11,29 +12,39 @@ export default function MinExperience() {
             </ScrollReveal>
             <div className="smaller-cards">
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Content and Branding</h2>
                     <h3 className="short-hook">June 2025 - Current</h3>
                     <p className="showcase">Python, n8n, Excel, Powerpoint</p>
+                    </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Full-Stack Developer</h2>
                     <h3 className="short-hook">March 2025 - Aug 2025</h3>
                     <p className="showcase">Next.js, PayloadCMS, Supabase, PostgreSQL, Git</p>
+                    </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Python Developer</h2>
                     <h3 className="short-hook">September 2024 - April 2025</h3>
                     <p className="showcase">Python, Pandas, Excel, Tkinter</p>
+                    </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Junior Cloud Engineer</h2>
                     <h3 className="short-hook">May 2023 - July 2024</h3>
                     <p className="showcase">Java, Go, Kubernetes, Docker, Git</p>
+                    </SequentialFadeIn>
                 </TiltCard>                
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Python Developer Intern</h2>
                     <h3 className="short-hook">May 2022 - August 2022</h3>
                     <p className="showcase">Python, Selenium, (MS)SQL</p>
+                    </SequentialFadeIn>
                 </TiltCard>
             </div>
         </>
--- src/components/minProjects.jsx
+++ src/components/minProjects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ScrollReveal from "./verticalScrollReveal";
+import SequentialFadeIn from "./sequentialFadeIn";
 import SlideInText from "./slideInAnimation";
 import TiltCard from "./gridAnimation";
 import Link from "next/link";
@@ -12,24 +13,34 @@ export default function MinProjects() {
             </ScrollReveal>
             <div className="smaller-cards">
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">AI Model for Adaptive Traffic Control</h2>
                     <p className="showcase">Python, PyTorch, SUMO, MatPlotLib</p>
+                    </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Cyclistic Data Analysis</h2>
                     <p className="showcase">Python, SQL, Excel, PowerPoint, Tableau <Link href="https://github.com/apnatvar/analytics/tree/main/Cyclistic" target="_blank">- Project Link</Link></p>
+                    </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">S&P500 Dashboard</h2>
                     <p className="showcase">R, RShiny, DPLYR, RStudio, Plotly <Link href="https://github.com/apnatvar/analytics/tree/main/Shiny_Stock" target="_blank">Project Link</Link></p>
+                    </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Morse Code Game</h2>
                     <p className="showcase">ARM, C, RaspberryPi, CMake, Doxygen <Link href="https://github.com/apnatvar/Microprocessor-Raspberry-Pi-Pico/tree/main/Pi%20Pico%20Coding/assign02" target="_blank">Project Link</Link></p>
+                    </SequentialFadeIn>
                 </TiltCard>                
                 <TiltCard>
+                    <SequentialFadeIn>
                     <h2 className="name">Blockchain based Emission Logger</h2>
                     <p className="showcase">Python, SQLite, Socket Programming <Link href="https://github.com/apnatvar/Computer-Networking/tree/main/Project%202" target="_blank">Project Link</Link></p>
+                    </SequentialFadeIn>
                 </TiltCard>
             </div>
         </>
