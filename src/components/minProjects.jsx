a42c04a65f1257ab30de8386dc3f636ae47f7837
apnatvar <arawat@tcd.ie>
2025-08-28 11:27:17 +0530

    updated projets to add code link on a separate line, updated scrollGrid CSS

--- src/components/minProjects.jsx
+++ src/components/minProjects.jsx
@@ -15,31 +15,36 @@ export default function MinProjects() {
                 <TiltCard>
                     <SequentialFadeIn>
                     <h2 className="name">AI Model for Adaptive Traffic Control</h2>
+                    <h3 className="short-hook"><Link href="https://github.com/apnatvar" target="_blank">See Code</Link></h3>
                     <p className="showcase">Python, PyTorch, SUMO, MatPlotLib</p>
                     </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
                     <SequentialFadeIn>
                     <h2 className="name">Cyclistic Data Analysis</h2>
-                    <p className="showcase">Python, SQL, Excel, PowerPoint, Tableau <Link href="https://github.com/apnatvar/analytics/tree/main/Cyclistic" target="_blank">- Project Link</Link></p>
+                    <h3 className="short-hook"><Link href="https://github.com/apnatvar/analytics/tree/main/Cyclistic" target="_blank">See Code</Link></h3>
+                    <p className="showcase">Python, SQL, Excel, PowerPoint, Tableau</p>
                     </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
                     <SequentialFadeIn>
                     <h2 className="name">S&P500 Dashboard</h2>
-                    <p className="showcase">R, RShiny, DPLYR, RStudio, Plotly <Link href="https://github.com/apnatvar/analytics/tree/main/Shiny_Stock" target="_blank">Project Link</Link></p>
+                    <h3 className="short-hook"><Link href="https://github.com/apnatvar/analytics/tree/main/Shiny_Stock" target="_blank">See Code</Link></h3>
+                    <p className="showcase">R, RShiny, DPLYR, RStudio, Plotly</p>
                     </SequentialFadeIn>
                 </TiltCard>
                 <TiltCard>
                     <SequentialFadeIn>
                     <h2 className="name">Morse Code Game</h2>
-                    <p className="showcase">ARM, C, RaspberryPi, CMake, Doxygen <Link href="https://github.com/apnatvar/Microprocessor-Raspberry-Pi-Pico/tree/main/Pi%20Pico%20Coding/assign02" target="_blank">Project Link</Link></p>
+                    <h3 className="short-hook"><Link href="https://github.com/apnatvar/Microprocessor-Raspberry-Pi-Pico/tree/main/Pi%20Pico%20Coding/assign02" target="_blank">See Code</Link></h3>
+                    <p className="showcase">ARM, C, RaspberryPi, CMake, Doxygen</p>
                     </SequentialFadeIn>
                 </TiltCard>                
                 <TiltCard>
                     <SequentialFadeIn>
                     <h2 className="name">Blockchain based Emission Logger</h2>
-                    <p className="showcase">Python, SQLite, Socket Programming <Link href="https://github.com/apnatvar/Computer-Networking/tree/main/Project%202" target="_blank">Project Link</Link></p>
+                    <h3 className="short-hook"><Link href="https://github.com/apnatvar/Computer-Networking/tree/main/Project%202" target="_blank">See Code</Link></h3>
+                    <p className="showcase">Python, SQLite, Socket Programming</p>
                     </SequentialFadeIn>
                 </TiltCard>
             </div>
